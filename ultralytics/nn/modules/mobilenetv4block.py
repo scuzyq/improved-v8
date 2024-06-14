c310dadc5b3ab9f42a32c6f6759db0a769f68731
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-06-14 14:58:08 +0800

    Create mobilenetv4block.py

---
+++ ultralytics/nn/modules/mobilenetv4block.py
@@ -0,0 +1,114 @@
+from typing import Any, Callable, Dict, List, Mapping, Optional, Tuple, Union
+ 
+import torch
+import torch.nn as nn
+ 
+from ultralytics.nn.modules import (Conv,C3, Bottleneck,C2f)
+ 
+def make_divisible(
+        value: float,
+        divisor: int,
+        min_value: Optional[float] = None,
+        round_down_protect: bool = True,
+    ) -> int:
+    """
+    This function is copied from here 
+    "https://github.com/tensorflow/models/blob/master/official/vision/modeling/layers/nn_layers.py"
+    
+    This is to ensure that all layers have channels that are divisible by 8.
+    Args:
+        value: A `float` of original value.
+        divisor: An `int` of the divisor that need to be checked upon.
+        min_value: A `float` of  minimum value threshold.
+        round_down_protect: A `bool` indicating whether round down more than 10%
+        will be allowed.
+    Returns:
+        The adjusted value in `int` that is divisible against divisor.
+    """
+    if min_value is None:
+        min_value = divisor
+    new_value = max(min_value, int(value + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if round_down_protect and new_value < 0.9 * value:
+        new_value += divisor
+    return int(new_value)
+ 
+def conv_2d(inp, oup, kernel_size=3, stride=1, groups=1, bias=False, norm=True, act=True):
+    conv = nn.Sequential()
+    padding = (kernel_size - 1) // 2
+    conv.add_module('conv', nn.Conv2d(inp, oup, kernel_size, stride, padding, bias=bias, groups=groups))
+    if norm:
+        conv.add_module('BatchNorm2d', nn.BatchNorm2d(oup))
+    if act:
+        conv.add_module('Activation', nn.ReLU6())
+    return conv
+ 
+class InvertedResidual(nn.Module):
+    def __init__(self, inp, oup, stride, expand_ratio, act=False):
+        super(InvertedResidual, self).__init__()
+        self.stride = stride
+        assert stride in [1, 2]
+        hidden_dim = int(round(inp * expand_ratio))
+        self.block = nn.Sequential()
+        if expand_ratio != 1:
+            self.block.add_module('exp_1x1', conv_2d(inp, hidden_dim, kernel_size=1, stride=1))
+        self.block.add_module('conv_3x3', conv_2d(hidden_dim, hidden_dim, kernel_size=3, stride=stride, groups=hidden_dim))
+        self.block.add_module('red_1x1', conv_2d(hidden_dim, oup, kernel_size=1, stride=1, act=act))
+        self.use_res_connect = self.stride == 1 and inp == oup
+ 
+    def forward(self, x):
+        if self.use_res_connect:
+            return x + self.block(x)
+        else:
+            return self.block(x)
+ 
+class UniversalInvertedBottleneckBlock(nn.Module):
+    def __init__(self, 
+            inp, 
+            oup, 
+            start_dw_kernel_size=0,
+            middle_dw_kernel_size=3,
+            middle_dw_downsample=True,
+            stride=1,
+            expand_ratio=2
+        ):
+        super().__init__()
+        # Starting depthwise conv.
+        self.start_dw_kernel_size = start_dw_kernel_size
+        if self.start_dw_kernel_size:            
+            stride_ = stride if not middle_dw_downsample else 1
+            self._start_dw_ = conv_2d(inp, inp, kernel_size=start_dw_kernel_size, stride=stride_, groups=inp, act=False)
+        # Expansion with 1x1 convs.
+        expand_filters = make_divisible(inp * expand_ratio, 8)
+        self._expand_conv = conv_2d(inp, expand_filters, kernel_size=1)
+        # Middle depthwise conv.
+        self.middle_dw_kernel_size = middle_dw_kernel_size
+        if self.middle_dw_kernel_size:
+            stride_ = stride if middle_dw_downsample else 1
+            self._middle_dw = conv_2d(expand_filters, expand_filters, kernel_size=middle_dw_kernel_size, stride=stride_, groups=expand_filters)
+        # Projection with 1x1 convs.
+        self._proj_conv = conv_2d(expand_filters, oup, kernel_size=1, stride=1, act=False)
+        
+        # Ending depthwise conv.
+        # this not used
+        # _end_dw_kernel_size = 0
+        # self._end_dw = conv_2d(oup, oup, kernel_size=_end_dw_kernel_size, stride=stride, groups=inp, act=False)
+        
+    def forward(self, x):
+        if self.start_dw_kernel_size:
+            x = self._start_dw_(x)
+            # print("_start_dw_", x.shape)
+        x = self._expand_conv(x)
+        # print("_expand_conv", x.shape)
+        if self.middle_dw_kernel_size:
+            x = self._middle_dw(x)
+            # print("_middle_dw", x.shape)
+        x = self._proj_conv(x)
+        # print("_proj_conv", x.shape)
+        return x
+ 
+ 
+class C2f_UIB(C2f):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(UniversalInvertedBottleneckBlock(self.c, self.c) for _ in range(n))
