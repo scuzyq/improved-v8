d7e398d306f191ef583b29d02fa72c8c73f6193f
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-06-17 08:55:25 +0800

    Update conv.py

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn as nn
 
 __all__ = ('Conv', 'Conv2', 'LightConv', 'DWConv', 'DWConvTranspose2d', 'ConvTranspose', 'Focus', 'GhostConv',
-           'ChannelAttention', 'SpatialAttention', 'CBAM', 'Concat', 'RepConv',''DySnakeConv'')
+           'ChannelAttention', 'SpatialAttention', 'CBAM', 'Concat', 'RepConv','DySnakeConv')
 
 
 def autopad(k, p=None, d=1):  # kernel, padding, dilation
