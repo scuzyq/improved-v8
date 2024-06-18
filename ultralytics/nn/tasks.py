fc9e4bb6bb6e77be73e9c1cdc3c089fe0859b67d
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-06-18 10:01:42 +0800

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -719,7 +719,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
                 c2 = make_divisible(min(c2, max_channels) * width, 8)
 
             args = [c1, c2, *args[1:]]
-            if m in (BottleneckCSP, C1, C2, C2f, C3, C3TR, C3Ghost, C3x, RepC3, C2f_Att, C2f_DCN, C2f_UIB, C2f_deformable_LKA, SKAttention, C2f_DySnakeConv,EMA_attention, SCDown):
+            if m in (BottleneckCSP, C1, C2, C2f, C3, C3TR, C3Ghost, C3x, RepC3, C2f_Att, C2f_DCN, C2f_UIB, C2f_deformable_LKA, SKAttention, C2f_DySnakeConv,EMA_attention):
                 args.insert(2, n)  # number of repeats
                 n = 1
         elif m is AIFI:
