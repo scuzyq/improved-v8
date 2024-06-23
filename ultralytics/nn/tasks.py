49f014e48758932a031edcd175fc72f76f75d612
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-06-23 20:54:53 +0800

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -45,6 +45,8 @@ from ultralytics.nn.modules.Detect_DBB import Detect_DBB
 from ultralytics.nn.modules.AFPNHead3 import Detect_AFPN3
 
 
+from ultralytics.nn.modules.CLLAHead import CLLAHead
+
 from ultralytics.nn.DEANet import CGAFusion
 
 from ultralytics.nn.modules.conv import EMA_attention
@@ -237,7 +239,7 @@ class BaseModel(nn.Module):
         """
         self = super()._apply(fn)
         m = self.model[-1]  # Detect()
-        if isinstance(m, (Detect, Detect_improve, Detect_AFPN, Detect_ASFF, Detect_FRM, ImplicitHead, AsDDet, LADH, Detect_DBB, Detect_AFPN3, Segment)):
+        if isinstance(m, (Detect, Detect_improve, Detect_AFPN, Detect_ASFF, Detect_FRM, ImplicitHead, AsDDet, LADH, Detect_DBB, Detect_AFPN3, CLLAHead, Segment)):
             m.stride = fn(m.stride)
             m.anchors = fn(m.anchors)
             m.strides = fn(m.strides)
@@ -296,7 +298,7 @@ class DetectionModel(BaseModel):
 
         # Build strides
         m = self.model[-1]  # Detect()
-        if isinstance(m, (Detect, Detect_improve, Detect_AFPN, Detect_ASFF, Detect_FRM, AsDDet,  ImplicitHead, LADH, Detect_DBB, Detect_AFPN3, Segment, Pose)):
+        if isinstance(m, (Detect, Detect_improve, Detect_AFPN, Detect_ASFF, Detect_FRM, AsDDet,  ImplicitHead, LADH, Detect_DBB, Detect_AFPN3, CLLAHead, Segment, Pose)):
             s = 256  # 2x min stride
             m.inplace = self.inplace
             forward = lambda x: self.forward(x)[0] if isinstance(m, (Segment, Pose)) else self.forward(x)
@@ -666,7 +668,7 @@ def attempt_load_weights(weights, device=None, inplace=True, fuse=False):
     # Module updates
     for m in ensemble.modules():
         t = type(m)
-        if t in (nn.Hardswish, nn.LeakyReLU, nn.ReLU, nn.ReLU6, nn.SiLU, Detect, Detect_improve, Detect_AFPN, Detect_ASFF, Detect_FRM, AsDDet, ImplicitHead, LADH, Detect_DBB, Detect_AFPN3, Segment):
+        if t in (nn.Hardswish, nn.LeakyReLU, nn.ReLU, nn.ReLU6, nn.SiLU, Detect, Detect_improve, Detect_AFPN, Detect_ASFF, Detect_FRM, AsDDet, ImplicitHead, LADH, Detect_DBB, Detect_AFPN3, CLLAHead, Segment):
             m.inplace = inplace
         elif t is nn.Upsample and not hasattr(m, 'recompute_scale_factor'):
             m.recompute_scale_factor = None  # torch 1.11.0 compatibility
@@ -702,7 +704,7 @@ def attempt_load_one_weight(weight, device=None, inplace=True, fuse=False):
     # Module updates
     for m in model.modules():
         t = type(m)
-        if t in (nn.Hardswish, nn.LeakyReLU, nn.ReLU, nn.ReLU6, nn.SiLU, Detect, Detect_improve, Detect_AFPN, Detect_ASFF, Detect_FRM, AsDDet, ImplicitHead, LADH, Detect_DBB, Detect_AFPN3, Segment):
+        if t in (nn.Hardswish, nn.LeakyReLU, nn.ReLU, nn.ReLU6, nn.SiLU, Detect, Detect_improve, Detect_AFPN, Detect_ASFF, Detect_FRM, AsDDet, ImplicitHead, LADH, Detect_DBB, Detect_AFPN3, CLLAHead, Segment):
             m.inplace = inplace
         elif t is nn.Upsample and not hasattr(m, 'recompute_scale_factor'):
             m.recompute_scale_factor = None  # torch 1.11.0 compatibility
@@ -770,7 +772,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
         elif m is CGAFusion:
             c2 = ch[f[1]]
             args = [c2, *args]
-        elif m in (Detect, Detect_improve, Detect_AFPN, Detect_ASFF, Detect_FRM, AsDDet, ImplicitHead, LADH, Detect_DBB, Detect_AFPN3, Segment, Pose):
+        elif m in (Detect, Detect_improve, Detect_AFPN, Detect_ASFF, Detect_FRM, AsDDet, ImplicitHead, LADH, Detect_DBB, Detect_AFPN3, CLLAHead, Segment, Pose):
             args.append([ch[x] for x in f])
             if m is Segment:
                 args[2] = make_divisible(min(args[2], max_channels) * width, 8)
@@ -887,7 +889,7 @@ def guess_model_task(model):
                 return cfg2task(eval(x))
 
         for m in model.modules():
-            if isinstance(m, (Detect, Detect_improve, Detect_AFPN, Detect_ASFF, Detect_FRM, AsDDet, ImplicitHead, LADH, Detect_DBB, Detect_AFPN3)):
+            if isinstance(m, (Detect, Detect_improve, Detect_AFPN, Detect_ASFF, Detect_FRM, AsDDet, ImplicitHead, LADH, Detect_DBB, Detect_AFPN3, CLLAHead)):
                 return 'detect'
             elif isinstance(m, Segment):
                 return 'segment'
