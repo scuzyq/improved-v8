c3fb6c9d105fbb7b5fd361566285fad84c12aade
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-07-27 17:25:07 +0800

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -13,7 +13,7 @@ from .transformer import TransformerBlock
 
 
 __all__ = ('DFL', 'HGBlock', 'HGStem', 'SPP', 'SPPF', 'C1', 'C2', 'C3', 'C2f', 'C3x', 'C3TR', 'C3Ghost',
-           'GhostBottleneck', 'Bottleneck', 'BottleneckCSP', 'Proto', 'RepC3', 'ResNetLayer', 'C2f_Att','C2f_DCN2','BiFPN_Concat2', 'BiFPN_Concat3')
+           'GhostBottleneck', 'Bottleneck', 'BottleneckCSP', 'Proto', 'RepC3', 'ResNetLayer', 'C2f_Att','C2f_DCN2','BiFPN_Concat2', 'BiFPN_Concat3','SPDConv', 'CSPOmniKernel')
 
 
 class DFL(nn.Module):
@@ -677,6 +677,84 @@ class BiFPN_Concat3(nn.Module):
         # Fast normalized fusion
         x = [weight[0] * x[0], weight[1] * x[1], weight[2] * x[2]]
         return torch.cat(x, self.d)
+
+
+class SPDConv(nn.Module):
+    # Changing the dimension of the Tensor
+    def __init__(self, inc, ouc, dimension=1):
+        super().__init__()
+        self.d = dimension
+        self.conv = Conv(inc * 4, ouc, k=3)
+
+    def forward(self, x):
+        x = torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1)
+        x = self.conv(x)
+        return x
+
+
+class OmniKernel(nn.Module):
+    def __init__(self, dim) -> None:
+        super().__init__()
+
+        ker = 31
+        pad = ker // 2
+        self.in_conv = nn.Sequential(
+                    nn.Conv2d(dim, dim, kernel_size=1, padding=0, stride=1),
+                    nn.GELU()
+                    )
+        self.out_conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0, stride=1)
+        self.dw_13 = nn.Conv2d(dim, dim, kernel_size=(1,ker), padding=(0,pad), stride=1, groups=dim)
+        self.dw_31 = nn.Conv2d(dim, dim, kernel_size=(ker,1), padding=(pad,0), stride=1, groups=dim)
+        self.dw_33 = nn.Conv2d(dim, dim, kernel_size=ker, padding=pad, stride=1, groups=dim)
+        self.dw_11 = nn.Conv2d(dim, dim, kernel_size=1, padding=0, stride=1, groups=dim)
+
+        self.act = nn.ReLU()
+
+        ### sca ###
+        self.conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.pool = nn.AdaptiveAvgPool2d((1,1))
+
+        ### fca ###
+        self.fac_conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.fac_pool = nn.AdaptiveAvgPool2d((1,1))
+        self.fgm = FGM(dim)
+
+    def forward(self, x):
+        out = self.in_conv(x)
+
+        ### fca ###
+        x_att = self.fac_conv(self.fac_pool(out))
+        x_fft = torch.fft.fft2(out, norm='backward')
+        x_fft = x_att * x_fft
+        x_fca = torch.fft.ifft2(x_fft, dim=(-2,-1), norm='backward')
+        x_fca = torch.abs(x_fca)
+
+        ### fca ###
+        ### sca ###
+        x_att = self.conv(self.pool(x_fca))
+        x_sca = x_att * x_fca
+        ### sca ###
+        x_sca = self.fgm(x_sca)
+
+        out = x + self.dw_13(out) + self.dw_31(out) + self.dw_33(out) + self.dw_11(out) + x_sca
+        out = self.act(out)
+        return self.out_conv(out)
+
+
+
+
+
+class CSPOmniKernel(nn.Module):
+    def __init__(self, dim, e=0.25):
+        super().__init__()
+        self.e = e
+        self.cv1 = Conv(dim, dim, 1)
+        self.cv2 = Conv(dim, dim, 1)
+        self.m = OmniKernel(int(dim * self.e))
+
+    def forward(self, x):
+        ok_branch, identity = torch.split(self.cv1(x), [int(x.size(1) * self.e), int(x.size(1) * (1 - self.e))], dim=1)
+        return self.cv2(torch.cat((self.m(ok_branch), identity), 1))
 
 
 # class C2f_VSS(C2f):
