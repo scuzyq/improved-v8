26760e4e27aa8a5850ab776d3221f4c000cd7540
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-08-03 16:40:38 +0800

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -113,6 +113,9 @@ from ultralytics.nn.modules.att import MCALayer
 from ultralytics.nn.neck.GFPN import CSPStage
 
 
+from ultralytics.nn.modules.kan_conv import C2f_KAN
+
+
 from ultralytics.nn.modules.att import SKAttention
 
 from ultralytics.nn.modules.att import C2f_deformable_LKA
@@ -805,13 +808,13 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
 
         n = n_ = max(round(n * depth), 1) if n > 1 else n  # depth gain
         if m in (Classify, Conv, ConvTranspose, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, SPDConv, DWConv, Focus,
-                 BottleneckCSP, C1, C2, C2f, C3, C3TR, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x, RepC3, C2f_Att, C2f_DCN, NAMAttention, PSA, C2f_UIB, BasicRFB, MCALayer, CSPStage, C2f_deformable_LKA, SKAttention, C2f_DySnakeConv,EMA_attention, SCDown, C2f_MSBlock, RepNCSPELAN4_low, RepNCSPELAN4_high, RCSOSA, C2f_MLCA, C2f_DCNv3_DLKA, C2fMLLABlock, C2f_Dual, C2f_DCN2):
+                 BottleneckCSP, C1, C2, C2f, C3, C3TR, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x, RepC3, C2f_Att, C2f_DCN, NAMAttention, PSA, C2f_UIB, BasicRFB, MCALayer, CSPStage, C2f_deformable_LKA, SKAttention, C2f_DySnakeConv,EMA_attention, SCDown, C2f_MSBlock, RepNCSPELAN4_low, RepNCSPELAN4_high, RCSOSA, C2f_MLCA, C2f_DCNv3_DLKA, C2fMLLABlock, C2f_Dual, C2f_DCN2, C2f_KAN):
             c1, c2 = ch[f], args[0]
             if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
                 c2 = make_divisible(min(c2, max_channels) * width, 8)
 
             args = [c1, c2, *args[1:]]
-            if m in (BottleneckCSP, C1, C2, C2f, C3, C3TR, C3Ghost, C3x, RepC3, C2f_Att, C2f_DCN, C2f_UIB, C2f_deformable_LKA, SKAttention, C2f_DySnakeConv,EMA_attention, C2f_MSBlock, C2f_MLCA, C2f_DCNv3_DLKA, C2fMLLABlock, C2f_Dual, C2f_DCN2):
+            if m in (BottleneckCSP, C1, C2, C2f, C3, C3TR, C3Ghost, C3x, RepC3, C2f_Att, C2f_DCN, C2f_UIB, C2f_deformable_LKA, SKAttention, C2f_DySnakeConv,EMA_attention, C2f_MSBlock, C2f_MLCA, C2f_DCNv3_DLKA, C2fMLLABlock, C2f_Dual, C2f_DCN2, C2f_KAN):
                 args.insert(2, n)  # number of repeats
                 n = 1
         elif m is AIFI:
