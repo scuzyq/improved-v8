4552be81271336c51ab2b0e983241c39b4a7b995
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-08-07 22:10:04 +0800

    Create dysampleSCAM.py

---
+++ ultralytics/nn/modules/dysampleSCAM.py
@@ -0,0 +1,133 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+ 
+from timm.models.layers import DropPath
+ 
+from ultralytics.nn.modules.conv import Conv,autopad
+ 
+ 
+from mmengine.model import constant_init, normal_init
+ 
+#from ultralytics.nn.modules import (Conv, C3, Bottleneck, C2f)
+ 
+class DySample(nn.Module):
+    def __init__(self, in_channels, scale=2, style='lp', groups=4, dyscope=False):
+        super().__init__()
+        self.scale = scale
+        self.style = style
+        self.groups = groups
+        assert style in ['lp', 'pl']
+        if style == 'pl':
+            assert in_channels >= scale ** 2 and in_channels % scale ** 2 == 0
+        assert in_channels >= groups and in_channels % groups == 0
+ 
+        if style == 'pl':
+            in_channels = in_channels // scale ** 2
+            out_channels = 2 * groups
+        else:
+            out_channels = 2 * groups * scale ** 2
+ 
+        self.offset = nn.Conv2d(in_channels, out_channels, 1)
+        normal_init(self.offset, std=0.001)
+        if dyscope:
+            self.scope = nn.Conv2d(in_channels, out_channels, 1)
+            constant_init(self.scope, val=0.)
+ 
+        self.register_buffer('init_pos', self._init_pos())
+ 
+    def _init_pos(self):
+        h = torch.arange((-self.scale + 1) / 2, (self.scale - 1) / 2 + 1) / self.scale
+        return torch.stack(torch.meshgrid([h, h])).transpose(1, 2).repeat(1, self.groups, 1).reshape(1, -1, 1, 1)
+ 
+    def sample(self, x, offset):
+        B, _, H, W = offset.shape
+        offset = offset.view(B, 2, -1, H, W)
+        coords_h = torch.arange(H) + 0.5
+        coords_w = torch.arange(W) + 0.5
+        coords = torch.stack(torch.meshgrid([coords_w, coords_h])
+                             ).transpose(1, 2).unsqueeze(1).unsqueeze(0).type(x.dtype).to(x.device)
+        normalizer = torch.tensor([W, H], dtype=x.dtype, device=x.device).view(1, 2, 1, 1, 1)
+        coords = 2 * (coords + offset) / normalizer - 1
+        coords = F.pixel_shuffle(coords.view(B, -1, H, W), self.scale).view(
+            B, 2, -1, self.scale * H, self.scale * W).permute(0, 2, 3, 4, 1).contiguous().flatten(0, 1)
+        return F.grid_sample(x.reshape(B * self.groups, -1, H, W), coords, mode='bilinear',
+                             align_corners=False, padding_mode="border").view(B, -1, self.scale * H, self.scale * W)
+ 
+    def forward_lp(self, x):
+        if hasattr(self, 'scope'):
+            offset = self.offset(x) * self.scope(x).sigmoid() * 0.5 + self.init_pos
+        else:
+            offset = self.offset(x) * 0.25 + self.init_pos
+        return self.sample(x, offset)
+ 
+    def forward_pl(self, x):
+        x_ = F.pixel_shuffle(x, self.scale)
+        if hasattr(self, 'scope'):
+            offset = F.pixel_unshuffle(self.offset(x_) * self.scope(x_).sigmoid(), self.scale) * 0.5 + self.init_pos
+        else:
+            offset = F.pixel_unshuffle(self.offset(x_), self.scale) * 0.25 + self.init_pos
+        return self.sample(x, offset)
+ 
+    def forward(self, x):
+        if self.style == 'pl':
+            return self.forward_pl(x)
+        return self.forward_lp(x)
+ 
+ 
+ 
+ 
+ 
+class Conv_withoutBN(nn.Module):
+    # Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)
+    default_act = nn.SiLU()  # default activation
+ 
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+ 
+    def forward(self, x):
+        return self.act(self.conv(x))
+ 
+ 
+class SCAM(nn.Module):
+    def __init__(self, in_channels, reduction=1):
+        super(SCAM, self).__init__()
+        self.in_channels = in_channels
+        self.inter_channels = in_channels
+ 
+        self.k = Conv(in_channels, 1, 1, 1)
+        self.v = Conv(in_channels, self.inter_channels, 1, 1)
+        self.m = Conv_withoutBN(self.inter_channels, in_channels, 1, 1)
+        self.m2 = Conv(2, 1, 1, 1)
+ 
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)  # GAP
+        self.max_pool = nn.AdaptiveMaxPool2d(1)  # GMP
+ 
+    def forward(self, x):
+        n, c, h, w = x.size(0), x.size(1), x.size(2), x.size(3)
+ 
+        # avg max: [N, C, 1, 1]
+        avg = self.avg_pool(x).softmax(1).view(n, 1, 1, c)
+        max = self.max_pool(x).softmax(1).view(n, 1, 1, c)
+ 
+        # k: [N, 1, HW, 1]
+        k = self.k(x).view(n, 1, -1, 1).softmax(2)
+ 
+        # v: [N, 1, C, HW]
+        v = self.v(x).view(n, 1, c, -1)
+ 
+        # y: [N, C, 1, 1]
+        y = torch.matmul(v, k).view(n, c, 1, 1)
+ 
+        # y2:[N, 1, H, W]
+        y_avg = torch.matmul(avg, v).view(n, 1, h, w)
+        y_max = torch.matmul(max, v).view(n, 1, h, w)
+ 
+        # y_cat:[N, 2, H, W]
+        y_cat = torch.cat((y_avg, y_max), 1)
+ 
+        y = self.m(y) * self.m2(y_cat).sigmoid()
+ 
+        return x + y
