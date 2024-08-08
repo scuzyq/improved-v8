5aea2886779e2d791ee463e5b091d9c265f71bb1
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-08-08 18:56:28 +0800

    Create DEANet.py

---
+++ ultralytics/nn/modules/DEANet.py
@@ -0,0 +1,68 @@
+import torch
+from torch import nn
+from einops import rearrange
+ 
+class SpatialAttention_CGA(nn.Module):
+    def __init__(self):
+        super(SpatialAttention_CGA, self).__init__()
+        self.sa = nn.Conv2d(2, 1, 7, padding=3, padding_mode='reflect' ,bias=True)
+ 
+    def forward(self, x):
+        x_avg = torch.mean(x, dim=1, keepdim=True)
+        x_max, _ = torch.max(x, dim=1, keepdim=True)
+        x2 = torch.concat([x_avg, x_max], dim=1)
+        sattn = self.sa(x2)
+        return sattn
+ 
+ 
+class ChannelAttention_CGA(nn.Module):
+    def __init__(self, dim, reduction = 8):
+        super(ChannelAttention_CGA, self).__init__()
+        self.gap = nn.AdaptiveAvgPool2d(1)
+        self.ca = nn.Sequential(
+            nn.Conv2d(dim, dim // reduction, 1, padding=0, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(dim // reduction, dim, 1, padding=0, bias=True),
+        )
+ 
+    def forward(self, x):
+        x_gap = self.gap(x)
+        cattn = self.ca(x_gap)
+        return cattn
+ 
+    
+class PixelAttention_CGA(nn.Module):
+    def __init__(self, dim):
+        super(PixelAttention_CGA, self).__init__()
+        self.pa2 = nn.Conv2d(2 * dim, dim, 7, padding=3, padding_mode='reflect' ,groups=dim, bias=True)
+        self.sigmoid = nn.Sigmoid()
+ 
+    def forward(self, x, pattn1):
+        B, C, H, W = x.shape
+        x = x.unsqueeze(dim=2) # B, C, 1, H, W
+        pattn1 = pattn1.unsqueeze(dim=2) # B, C, 1, H, W
+        x2 = torch.cat([x, pattn1], dim=2) # B, C, 2, H, W
+        x2 = rearrange(x2, 'b c t h w -> b (c t) h w')
+        pattn2 = self.pa2(x2)
+        pattn2 = self.sigmoid(pattn2)
+        return pattn2
+ 
+class CGAFusion(nn.Module):
+    def __init__(self, dim, reduction=8):
+        super(CGAFusion, self).__init__()
+        self.sa = SpatialAttention_CGA()
+        self.ca = ChannelAttention_CGA(dim, reduction)
+        self.pa = PixelAttention_CGA(dim)
+        self.conv = nn.Conv2d(dim, dim, 1, bias=True)
+        self.sigmoid = nn.Sigmoid()
+ 
+    def forward(self, data):
+        x, y = data
+        initial = x + y
+        cattn = self.ca(initial)
+        sattn = self.sa(initial)
+        pattn1 = sattn + cattn
+        pattn2 = self.sigmoid(self.pa(initial, pattn1))
+        result = initial + pattn2 * x + (1 - pattn2) * y
+        result = self.conv(result)
+        return result
