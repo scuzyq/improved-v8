03eab0a05ed5a3289c49cf9fd0049dba9be6446b
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-08-21 16:31:28 +0800

    Create CPMS.py

---
+++ ultralytics/nn/modules/CPMS.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+ 
+ 
+class MSCAAttention(nn.Module):
+    # SegNext NeurIPS 2022
+    # https://github.com/Visual-Attention-Network/SegNeXt/tree/main
+    def __init__(self, dim):
+        super().__init__()
+        self.conv0 = nn.Conv2d(dim, dim,  5, padding=2, groups=dim)
+        self.conv0_1 = nn.Conv2d(dim, dim, (1, 7), padding=(0, 3), groups=dim)
+        self.conv0_2 = nn.Conv2d(dim, dim, (7, 1), padding=(3, 0), groups=dim)
+ 
+        self.conv1_1 = nn.Conv2d(dim, dim, (1, 11), padding=(0, 5), groups=dim)
+        self.conv1_2 = nn.Conv2d(dim, dim, (11, 1), padding=(5, 0), groups=dim)
+ 
+        self.conv2_1 = nn.Conv2d(dim, dim, (1, 21), padding=(0, 10), groups=dim)
+        self.conv2_2 = nn.Conv2d(dim, dim, (21, 1), padding=(10, 0), groups=dim)
+        self.conv3 = nn.Conv2d(dim, dim, 1)
+ 
+    def forward(self, x):
+        u = x.clone()
+        attn = self.conv0(x)
+ 
+        attn_0 = self.conv0_1(attn)
+        attn_0 = self.conv0_2(attn_0)
+ 
+        attn_1 = self.conv1_1(attn)
+        attn_1 = self.conv1_2(attn_1)
+ 
+        attn_2 = self.conv2_1(attn)
+        attn_2 = self.conv2_2(attn_2)
+        attn = attn + attn_0 + attn_1 + attn_2
+ 
+        attn = self.conv3(attn)
+ 
+        return attn * u
+ 
+ 
+class CPMS(nn.Module):
+    #BY https://blog.csdn.net/m0_63774211?type=lately
+    def __init__(self, channels):
+        super().__init__()
+ 
+        self.ca = MSCAAttention(channels)
+ 
+        self.dconv5_5 = nn.Conv2d(channels, channels, kernel_size=5, padding=2, groups=channels)
+        self.dconv1_7 = nn.Conv2d(channels, channels, kernel_size=(1, 7), padding=(0, 3), groups=channels)
+        self.dconv7_1 = nn.Conv2d(channels, channels, kernel_size=(7, 1), padding=(3, 0), groups=channels)
+        self.dconv1_11 = nn.Conv2d(channels, channels, kernel_size=(1, 11), padding=(0, 5), groups=channels)
+        self.dconv11_1 = nn.Conv2d(channels, channels, kernel_size=(11, 1), padding=(5, 0), groups=channels)
+        self.dconv1_21 = nn.Conv2d(channels, channels, kernel_size=(1, 21), padding=(0, 10), groups=channels)
+        self.dconv21_1 = nn.Conv2d(channels, channels, kernel_size=(21, 1), padding=(10, 0), groups=channels)
+        self.conv = nn.Conv2d(channels, channels, kernel_size=(1, 1), padding=0)
+        self.act = nn.GELU()
+ 
+    def forward(self, inputs):
+        #   Global Perceptron
+        inputs = self.conv(inputs)
+        inputs = self.act(inputs)
+ 
+        inputs = self.ca(inputs)
+ 
+        x_init = self.dconv5_5(inputs)
+        x_1 = self.dconv1_7(x_init)
+        x_1 = self.dconv7_1(x_1)
+        x_2 = self.dconv1_11(x_init)
+        x_2 = self.dconv11_1(x_2)
+        x_3 = self.dconv1_21(x_init)
+        x_3 = self.dconv21_1(x_3)
+        x = x_1 + x_2 + x_3 + x_init
+        spatial_att = self.conv(x)
+        out = spatial_att * inputs
+        out = self.conv(out)
+        return out
