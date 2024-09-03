9b2c22a49965a21c990ad48d1002105d3b77a5f3
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-09-03 21:50:46 +0800

    Update AdditiveBlock_CGLU.py

--- ultralytics/nn/modules/AdditiveBlock_CGLU.py
+++ ultralytics/nn/modules/AdditiveBlock_CGLU.py
@@ -8,7 +8,40 @@ from functools import partial
 from typing import Optional, Callable, Union
 from einops import rearrange
 from ultralytics.nn.modules.conv import Conv, DWConv, DSConv, RepConv, GhostConv, autopad
-from from ultralytics.nn.modules.block import *
+from ultralytics.nn.modules.block import *
+
+
+class ConvolutionalGLU(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.) -> None:
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        hidden_features = int(2 * hidden_features / 3)
+        self.fc1 = nn.Conv2d(in_features, hidden_features * 2, 1)
+        self.dwconv = nn.Sequential(
+            nn.Conv2d(hidden_features, hidden_features, kernel_size=3, stride=1, padding=1, bias=True, groups=hidden_features),
+            act_layer()
+        )
+        self.fc2 = nn.Conv2d(hidden_features, out_features, 1)
+        self.drop = nn.Dropout(drop)
+    
+    # def forward(self, x):
+    #     x, v = self.fc1(x).chunk(2, dim=1)
+    #     x = self.dwconv(x) * v
+    #     x = self.drop(x)
+    #     x = self.fc2(x)
+    #     x = self.drop(x)
+    #     return x
+
+    def forward(self, x):
+        x_shortcut = x
+        x, v = self.fc1(x).chunk(2, dim=1)
+        x = self.dwconv(x) * v
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x_shortcut + x
+
 
 ######################################## CAS-ViT start ########################################
 
