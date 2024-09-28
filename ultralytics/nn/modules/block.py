f84d2a0c34defc0e258c0278cd3f33ac4ef9fa31
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-09-28 22:15:30 +0800

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -867,6 +867,92 @@ class C2f_DeepDBB(C2f):
 
 
 
+######################################## SHViT CVPR2024 start ########################################
+
+class SHSA_GroupNorm(torch.nn.GroupNorm):
+    """
+    Group Normalization with 1 group.
+    Input: tensor in shape [B, C, H, W]
+    """
+    def __init__(self, num_channels, **kwargs):
+        super().__init__(1, num_channels, **kwargs)
+
+class SHSABlock_FFN(torch.nn.Module):
+    def __init__(self, ed, h):
+        super().__init__()
+        self.pw1 = Conv2d_BN(ed, h)
+        self.act = torch.nn.SiLU()
+        self.pw2 = Conv2d_BN(h, ed, bn_weight_init=0)
+
+    def forward(self, x):
+        x = self.pw2(self.act(self.pw1(x)))
+        return x
+
+class SHSA(torch.nn.Module):
+    """Single-Head Self-Attention"""
+    def __init__(self, dim, qk_dim, pdim):
+        super().__init__()
+        self.scale = qk_dim ** -0.5
+        self.qk_dim = qk_dim
+        self.dim = dim
+        self.pdim = pdim
+
+        self.pre_norm = SHSA_GroupNorm(pdim)
+
+        self.qkv = Conv2d_BN(pdim, qk_dim * 2 + pdim)
+        self.proj = torch.nn.Sequential(torch.nn.SiLU(), Conv2d_BN(
+            dim, dim, bn_weight_init = 0))
+        
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        x1, x2 = torch.split(x, [self.pdim, self.dim - self.pdim], dim = 1)
+        x1 = self.pre_norm(x1)
+        qkv = self.qkv(x1)
+        q, k, v = qkv.split([self.qk_dim, self.qk_dim, self.pdim], dim = 1)
+        q, k, v = q.flatten(2), k.flatten(2), v.flatten(2)
+        
+        attn = (q.transpose(-2, -1) @ k) * self.scale
+        attn = attn.softmax(dim = -1)
+        x1 = (v @ attn.transpose(-2, -1)).reshape(B, self.pdim, H, W)
+        x = self.proj(torch.cat([x1, x2], dim = 1))
+
+        return x
+
+class SHSABlock(torch.nn.Module):
+    def __init__(self, dim, qk_dim=16, pdim=64):
+        super().__init__()
+        self.conv = Residual(Conv2d_BN(dim, dim, 3, 1, 1, groups = dim, bn_weight_init = 0))
+        self.mixer = Residual(SHSA(dim, qk_dim, pdim))
+        self.ffn = Residual(SHSABlock_FFN(dim, int(dim * 2)))
+    
+    def forward(self, x):
+        return self.ffn(self.mixer(self.conv(x)))
+
+class C2f_SHSA(C2f):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(SHSABlock(self.c) for _ in range(n))
+
+class SHSABlock_CGLU(torch.nn.Module):
+    def __init__(self, dim, qk_dim=16, pdim=64):
+        super().__init__()
+        self.conv = Residual(Conv2d_BN(dim, dim, 3, 1, 1, groups = dim, bn_weight_init = 0))
+        self.mixer = Residual(SHSA(dim, qk_dim, pdim))
+        self.ffn = ConvolutionalGLU(dim, int(dim * 2))
+    
+    def forward(self, x):
+        return self.ffn(self.mixer(self.conv(x)))
+
+class C2f_SHSA_CGLU(C2f):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(SHSABlock_CGLU(self.c) for _ in range(n))
+
+######################################## SHViT CVPR2024 end ########################################
+
+
+
 
 # class C2f_VSS(C2f):
 #     def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
