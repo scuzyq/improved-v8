a05b0ea30d63c96a23de14552e83014937d011cf
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-11-24 19:44:31 +0800

    Create CMUNeXt.py

---
+++ ultralytics/nn/modules/CMUNeXt.py
@@ -0,0 +1,118 @@
+import torch
+import torch.nn as nn
+
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x):
+        return self.fn(x) + x
+
+
+class CMUNeXtBlock(nn.Module):
+    def __init__(self, ch_in, ch_out, k=3, s=2, depth=1):
+        super(CMUNeXtBlock, self).__init__()
+        self.block = nn.Sequential(
+            *[nn.Sequential(
+                Residual(nn.Sequential(
+                    # deep wise
+                    nn.Conv2d(ch_in, ch_in, kernel_size=(k, k), groups=ch_in, padding=(k // 2, k // 2)),
+                    nn.GELU(),
+                    nn.BatchNorm2d(ch_in)
+                )),
+                nn.Conv2d(ch_in, ch_in * 4, kernel_size=(1, 1)),
+                nn.GELU(),
+                nn.BatchNorm2d(ch_in * 4),
+                nn.Conv2d(ch_in * 4, ch_in, kernel_size=(1, 1)),
+                nn.GELU(),
+                nn.BatchNorm2d(ch_in)
+            ) for i in range(depth)]
+        )
+        self.up = Conv(ch_in, ch_out, k, s)
+
+    def forward(self, x):
+        x = self.block(x)
+        x = self.up(x)
+        return x
+
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+
+class Bottleneck_CMUNeXtB(nn.Module):
+    """Standard bottleneck."""
+
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        """Initializes a standard bottleneck module with optional shortcut connection and configurable parameters."""
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.cv2 = CMUNeXtBlock(c_, c2, 3, 1)
+        self.add = shortcut and c1 == c2
+
+    def forward(self, x):
+        """Applies the YOLO FPN to input data."""
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+
+
+class C2f_CMUNeXtB(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initializes a CSP bottleneck with 2 convolutions and n Bottleneck blocks for faster processing."""
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck_CMUNeXtB(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+
+if __name__ == '__main__':
+    CMUN = CMUNeXtBlock(256,128,3,2)
+    #创建一个输入张量
+    batch_size = 8
+    input_tensor=torch.randn(batch_size, 256, 64, 64 )
+    #运行模型并打印输入和输出的形状
+    output_tensor =CMUN(input_tensor)
+    print("Input shape:",input_tensor.shape)
+    print("0utput shape:",output_tensor.shape)
